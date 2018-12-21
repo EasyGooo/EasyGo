fac5e7bd835c351b3b578d743364b91d1d4d10d1
MiguelColo <mig_colo@yahoo.es>
2018-12-21 14:28:22 +0100

    finfaaaa

---
+++ server/public/precache-manifest.c9b56d0e8bcc85be8bec0c283aff1e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "158044a8695a66618fce",
+    "url": "/static/js/main.158044a8.chunk.js"
+  },
+  {
+    "revision": "d70f7b7cd4843b9c5012",
+    "url": "/static/js/1.d70f7b7c.chunk.js"
+  },
+  {
+    "revision": "158044a8695a66618fce",
+    "url": "/static/css/main.96c4014f.chunk.css"
+  },
+  {
+    "revision": "2a0a27fd0123f89c17d5eb4e62ddc930",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3941f80600f48af0f1d22e3b36825441.js"
+  "/precache-manifest.c9b56d0e8bcc85be8bec0c283aff1e81.js"
 );
 
 workbox.clientsClaim();
